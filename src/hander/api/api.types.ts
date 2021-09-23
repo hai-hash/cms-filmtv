7f52789bb7577962680ad46e77513a7e1b984e32
whynotme1131999 <whynotme1131999@gmail.com>
2021-09-23 19:55:09 +0700

    update

--- src/hander/api/api.types.ts
+++ src/hander/api/api.types.ts
@@ -1,5 +1,5 @@
-export const URL = "http://localhost:8080";
-// export const URL = "https://file-managementt.herokuapp.com/";
+// export const URL = "http://localhost:8080";
+export const URL = "https://file-managementt.herokuapp.com/";
 
 export interface dataReq<T>{
     data:T
